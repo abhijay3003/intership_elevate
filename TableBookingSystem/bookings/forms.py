f228c5585355f6e36215dc4fb85f6dee4793dfc1
abhijay3003 <abhijay9505@gmail.com>
2025-10-31 21:57:29 +0530

    Initial commit: K-Means clustering with Mall dataset

--- TableBookingSystem/bookings/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Booking
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ['table_number', 'booking_date', 'booking_time']
